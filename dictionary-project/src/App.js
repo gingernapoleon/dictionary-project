8f60eb6829958c9c8c46e5d6b3d46a02fc1b1e54
Michael Simms <81196688+gingernapoleon@users.noreply.github.com>
2021-07-07 01:29:53 -0700

    added container to App
    for use of bootstrap

--- dictionary-project/src/App.js
+++ dictionary-project/src/App.js
@@ -4,9 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <a href="a" className="btn btn-primary">Button</a>
       </header>
+    </div>
+    <footer className="text-footer">Coded by Ginger Napoleon</footer>
     </div>
   );
 }
