1ac9fec67867b6642fbe1bfef44dff25a392eb71
Michael Simms <81196688+gingernapoleon@users.noreply.github.com>
2021-07-07 01:24:55 -0700

    import bootstrap

--- dictionary-project/src/App.js
+++ dictionary-project/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <a href="a" className="btn btn-primary">Button</a>
       </header>
     </div>
   );
