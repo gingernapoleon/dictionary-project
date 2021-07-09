17337713f58f5f77e2a77321292d33493ac9c2b0
Michael Simms <81196688+gingernapoleon@users.noreply.github.com>
2021-07-08 22:35:22 -0700

    styling updated

--- dictionary-project/src/App.js
+++ dictionary-project/src/App.js
@@ -1,21 +1,20 @@
 import Dictionary from "./Dictionary";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">
-      </header>
-    </div>
-    <main>
-      <Dictionary />
-    </main>
-    <footer className="app-footer">
-      <small>Coded by Ginger Napoleon</small>
-      </footer>
+        <header className="App-header">
+          <h3>Dictionary</h3>
+        </header>
+        <main>
+          <Dictionary defaultKeyword="alien" />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by SheCodes</small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
